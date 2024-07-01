3378c3bb909b2b8785da542fe429615ddf92c110
reoring <reoring@craftsman-software.com>
2024-07-01 16:48:25 +0900

    chore: add atuin support

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -18,14 +18,19 @@ set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
 starship init fish | source
-
 direnv hook fish | source
+
+# atuin
+atuin init fish | source
+# bind to ctrl-r in normal and insert mode, add any other bindings you want here too
+bind \cr _atuin_search
+bind -M insert \cr _atuin_search
 
 function fish_greeting
     echo "Welcome to your modern fish shell with starship prompt!"
 end
 
 function fish_user_key_bindings
-    bind \cr 'history | fzf | read -l result; and commandline -- $result'
+    # bind \cr 'history | fzf | read -l result; and commandline -- $result'
     bind \ct 'fd --type f --hidden --follow --exclude .git | fzf | read -l result; and commandline -- $result'
 end
